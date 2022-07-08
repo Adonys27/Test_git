7674861ec29a6df41b673891cc1fadc02dea80f5
Adonys Coseri <adonys.coseri@fortech.ro>
2022-07-08 14:15:08 +0300

    am scos x

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
-        int x = 0;
+
         elev student = new elev();
         System.out.println("introdu un nume: ");
         student.name = cin.nextLine();
@@ -13,9 +13,9 @@ public class Main {
         System.out.println("introdu 5 note");
         for(int i=0;i<student.note.length;i++){
             student.note[i] = cin.nextFloat();
-            x++;
+
         }
-        System.out.println(x);
+
         student.afis();
     }
 }
